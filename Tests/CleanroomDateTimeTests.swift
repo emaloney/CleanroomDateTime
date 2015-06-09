c70b540d5b3355baa11e88e6adbc72142a2f5dac
Evan Maloney <emaloney@gilt.com>
2015-06-09 15:02:44 -0400

    initial code import

---
+++ Tests/CleanroomDateTimeTests.swift
@@ -0,0 +1,36 @@
+//
+//  CleanroomDateTimeTests.swift
+//  CleanroomDateTimeTests
+//
+//  Created by Evan Maloney on 6/9/15.
+//  Copyright (c) 2015 Gilt Groupe. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class CleanroomDateTimeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
