6be2e557f5150d2a5727aeb095853596217a0e8c
Evan Maloney <emaloney@gilt.com>
2016-10-17 17:18:46 -0400

    updating generated files

--- BuildControl/bin/travisBuildHelper.sh
+++ BuildControl/bin/travisBuildHelper.sh
@@ -62,7 +62,7 @@ while [[ $THIS_TRY < $MAXIMUM_TRIES ]]; do
 		echo "Attempt $THIS_TRY of $MAXIMUM_TRIES..."
 	fi
 	
-	( set -o pipefail && xcodebuild -project CleanroomDateTime.xcodeproj -configuration Release ONLY_ACTIVE_ARCH=YES -scheme CleanroomDateTime-$PLATFORM -destination "$DESTINATION" -destination-timeout 300 $XCODE_ACTION 2>&1 | tee "CleanroomDateTime-$PLATFORM-$OPERATION.log" | xcpretty )
+	( set -o pipefail && xcodebuild -project CleanroomDateTime.xcodeproj -configuration Debug -scheme CleanroomDateTime-$PLATFORM -destination "$DESTINATION" -destination-timeout 300 $XCODE_ACTION 2>&1 | tee "CleanroomDateTime-$PLATFORM-$OPERATION.log" | xcpretty )
 	XCODE_RESULT="${PIPESTATUS[0]}"
 	if [[ "$XCODE_RESULT" == "0" ]]; then
 		rm "CleanroomDateTime-$PLATFORM-$OPERATION.log"
